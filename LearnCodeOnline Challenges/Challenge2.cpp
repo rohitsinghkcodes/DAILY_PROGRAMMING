0f8658bf94041a1c2cfd6413e651ae21dd5726c6
Rohit Singh <conankmails@gmail.com>
2020-04-04 08:01:50 +0530

    Challenge 2 learncodeonline Cmpleted

---
+++ LearnCodeOnline Challenges/Challenge2.cpp
@@ -0,0 +1,33 @@
+//Challenge 2: Pattern printing
+
+#include<iostream>
+using namespace std;
+
+void patternPrint(int *n)
+{
+    for(int i=0;i<*n/2;i++)
+    {
+        for(int l=0;l<2;l++)
+        {
+            for(int j=(*n/2)-i;j>0;j--)
+                {
+                    cout<<"   ";
+                }
+        
+            for(int k=0;k<(2*i)+2;k++)
+                {
+                    cout<<"*  ";
+                }
+            cout<<endl<<endl;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the height of the tower= ";
+    cin>>n;
+    patternPrint(&n);
+    
+}
