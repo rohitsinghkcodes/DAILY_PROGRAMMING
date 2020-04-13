057ffb77d544206afde1a8df94f7e3fc40be128c
Rohit Singh <conankmails@gmail.com>
2020-04-13 11:52:02 +0530

    Challenge 11 completed: learnCodeOnline

---
+++ LearnCodeOnline Challenges/Challenge11.cpp
@@ -0,0 +1,13 @@
+//Challenge 11: Den rented two movies to watch last night.
+// The first was 100 min long, the second was 110 min long.
+// how many hours did it take for Dan to watch the two movies?
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int movie1Time=100,movie2Time=110;
+    cout<<"The time taken to watch 2 movies by Den is "<<float(movie1Time+movie2Time)/60<<" hours"<<endl;
+    return 0;
+}
