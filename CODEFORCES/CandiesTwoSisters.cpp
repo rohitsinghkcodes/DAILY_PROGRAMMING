0e285903eea638d6dcc92f0ae755ba7ec2ebffe4
Rohit Singh <conankmails@gmail.com>
2020-07-08 04:29:26 +0530

    commit1_1 on 08-07-2020

---
+++ CODEFORCES/CandiesTwoSisters.cpp
@@ -0,0 +1,17 @@
+//08-07-2020
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int no;
+        cin>>no;
+        cout<<(no-1)/2<<endl;
+    }
+    return 0;
+}
